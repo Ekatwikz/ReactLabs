b98fd4870c3057ceefa7c94e44b25de5b24e17b9
Emmanuel Katwikirize <ekatwikz@gmail.com>
2022-10-11 09:46:05 +0200

    small change

--- lab2/src/Components/PlayerInput/PlayerInput.js
+++ lab2/src/Components/PlayerInput/PlayerInput.js
@@ -1,7 +1,7 @@
 function PlayerInput(props) {
 	return (
 		<div className="PlayerInput">
-			<span>Set Name of Player {props.playerNum}: </span>
+			<span>Set Name of Player {props.playerNumStr}: </span>
 			<input 	type="text" value={props.playerName} onChange={e => props.changePlayerName(e.target.value)}/>
 		</div>
 	);
